51183cd51141d87c431ac0c0f052b29b94fa78d1
Niclas <spam@nicmar.nu>
2022-03-31 13:47:39 +0200

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "ENTRYPOINT.SH!"
 echo "-------------"
-echo "Hello $1"
-echo "Hello $2"
+echo "Hello $1 !"
+echo "Hello $2 ?"
 echo "::set-output name=username::Username $1"
 echo "::set-output name=password::Password $2"
 time=$(date)
